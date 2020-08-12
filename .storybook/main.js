ab6e2f36f2330bb887202d118fb69beaa08a180e
Mats Grimsgaard <mats@mgv.no>
2020-08-12 12:47:12 +0200

    Upgrade to storybook 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
-  addons: [
-    "@storybook/preset-typescript",
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-knobs/register",
-  ],
+  addons: ["@storybook/addon-knobs"],
 };
