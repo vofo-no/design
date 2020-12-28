5ac4ffca5454519488f7d44c2e4833858499381d
Mats Grimsgaard <mats@mgv.no>
2020-12-28 15:04:31 +0100

    Refactor Menu

--- src/components/Menu/Menu.stories.tsx
+++ src/components/Menu/Menu.stories.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Theme, MenuButton } from "../";
+import { MenuButton } from "../";
+import { Box } from "../Box";
 import { Menu, MenuContainer } from "./";
 
 export default { title: "Menu" };
@@ -7,21 +8,21 @@ export default { title: "Menu" };
 export const Default = () => {
   const [menu, setMenu] = useState(false);
   return (
-    <Theme>
+    <Box variant="light">
       <MenuButton open={menu} onClick={() => setMenu(!menu)} />
-      <MenuContainer>
-        <Menu open={menu}>
+      <MenuContainer open={menu}>
+        <Menu>
           <a href="#">Politikk</a>
           <a href="#">Medlemmer</a>
           <a href="#">Fakta</a>
           <a href="#">Ressurser</a>
         </Menu>
-        <Menu variant="top" open={menu}>
+        <Menu variant="top">
           <a href="#">English</a>
           <a href="#">Om Vofo</a>
           <a href="#">Kontakt oss</a>
         </Menu>
       </MenuContainer>
-    </Theme>
+    </Box>
   );
 };
