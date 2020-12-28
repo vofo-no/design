fdcc2a8f8a154b71fdb64861d4fd43d2c01897c5
Mats Grimsgaard <mats@mgv.no>
2020-12-28 15:05:10 +0100

    Add ScrollDown component

---
+++ src/components/ScrollDown/ScrollDown.stories.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Box } from "../Box";
+import { ScrollDown } from "./";
+
+export default {
+  title: "ScrollDown",
+};
+
+export const Default = () => (
+  <Box variant="light">
+    <h1>Scroll me</h1>
+    <ScrollDown />
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+    <p>Filler</p>
+  </Box>
+);
