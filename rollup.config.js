f6c96a46ef2820853b990d06cfba21afd8963db6
Mats Grimsgaard <mats@mgv.no>
2021-01-25 09:55:33 +0100

    Improve transpiled package

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
+import analyze from "rollup-plugin-analyzer";
+import { sizeSnapshot } from "rollup-plugin-size-snapshot";
 
 import pkg from "./package.json";
 
@@ -8,16 +11,32 @@ export default {
   input: "src/index.ts",
   output: [
     {
-      file: pkg.main,
+      file: "dist/index.js",
+      format: "umd",
+      name: "VofoDesign",
+      globals: {
+        react: "React",
+      },
+    },
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      name: "VofoDesign",
+    },
+    {
+      file: "dist/index.esm.js",
       format: "es",
     },
   ],
   external: [...Object.keys(pkg.peerDependencies || {})],
   plugins: [
+    peerDepsExternal(),
     nodeResolve(),
     commonjs(),
     typescript({
       typescript: require("typescript"),
     }),
+    analyze(),
+    sizeSnapshot(),
   ],
 };
